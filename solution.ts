675364823906c9fa211517ac6374fcd551c8e057
ArunRoy404 <arunroy0065@gmail.com>
2025-11-18 22:32:46 +0600

    feat: solution 2 added

--- solution.ts
+++ solution.ts
@@ -6,3 +6,19 @@ const formatValue = (
   if (typeof value === "boolean") return !value;
   return value;
 };
+
+
+
+
+
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") return value.length;
+  if (Array.isArray(value)) return value.length;
+  return 0;
+};
+
+
+
+
+
